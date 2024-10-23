30623e88c6ece560977b05c042bf1c349d5281b6
Muhammad Rehan <muhammadrehan125768@gmail.com>
2024-10-23 16:05:32 +0500

    remove unwanted things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import HeroSection from "@/components/HeroSection/HeroSection";
 import RecentPost from "@/components/RecentPostSection/RecentPost";
